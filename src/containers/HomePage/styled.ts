b6178023fa23c284db9e1df06bf01f9b87b556a2
Matheus Dario <dario.matheus97@gmail.com>
2023-12-09 00:06:41 -0300

    update responsivity

--- src/containers/HomePage/styled.ts
+++ src/containers/HomePage/styled.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const MainBody = styled.div`
-  background-color: ${({theme}) => theme.colors.primary};
 `;
 
 export const Container = styled.div`
--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -36,4 +36,3 @@ export const GlobalStyles = createGlobalStyle`
     text-decoration: 0;
   }
 `;
-
